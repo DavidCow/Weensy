6b4e0928c62217025a615eea224b98a7705d835e
David Cao <davidcaone@gmail.com>
2019-01-21 23:38:39 +0100

    n1 - react-native-video installed, 3 videos in 1 grid displayed and played simultaneously

--- src/scenes/MyWeensysPage/index.js
+++ src/scenes/MyWeensysPage/index.js
@@ -10,40 +10,81 @@ import {
   StyleSheet, 
   Text, 
   View,
+  ScrollView,
+  Dimensions
 } from 'react-native';
-
+import Video from 'react-native-video';
 
 
 export default class MyWeensysPage extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Here are my Weensys!</Text>
+      <ScrollView style={styles.scrollContainer}>
 
-        <View style={styles.body}>
-        
+        <View style={styles.container}>
+          <View style={styles.box}><Video source={{uri: "https://www.davidcaos.com/weensy/cap.mp4"}}   // Can be a URL or a local file.
+                ref={(ref) => {
+                  this.player = ref
+                }}                                      // Store reference
+                onBuffer={this.onBuffer}                // Callback when remote video is buffering
+                onError={this.videoError}               // Callback when video cannot be loaded
+                 style={styles.backgroundVideo} 
+                 repeat={false}/></View>
+                   <View style={styles.box}><Video source={{uri: "https://www.davidcaos.com/weensy/konohasenpu.mp4"}}   // Can be a URL or a local file.
+                ref={(ref) => {
+                  this.player = ref
+                }}                                      // Store reference
+                onBuffer={this.onBuffer}                // Callback when remote video is buffering
+                onError={this.videoError}               // Callback when video cannot be loaded
+                 style={styles.backgroundVideo} 
+                 repeat={false}/></View>
+                   <View style={styles.box}><Video source={{uri: "https://www.davidcaos.com/weensy/cap.mp4"}}   // Can be a URL or a local file.
+                ref={(ref) => {
+                  this.player = ref
+                }}                                      // Store reference
+                onBuffer={this.onBuffer}                // Callback when remote video is buffering
+                onError={this.videoError}               // Callback when video cannot be loaded
+                 style={styles.backgroundVideo} 
+                 repeat={false}/></View>
+                 
+          <View style={styles.box}><Text>Box 2</Text></View>
+          <View style={styles.box}><Text>Box 3</Text></View>
+          <View style={styles.box}><Text>Box 4</Text></View>
+          <View style={styles.box}><Text>Box 5</Text></View>
+          <View style={styles.box}><Text>Box 6</Text></View>
+          <View style={styles.box}><Text>Box 7</Text></View>
+          <View style={styles.box}><Text>Box 8</Text></View>
+          <View style={styles.box}><Text>Box 9</Text></View>
         </View>
 
-      </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flex: 1,
+  },
   container: {
-    flex: 1
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: 2,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  box: {
+    margin: 2,
+    width: Dimensions.get('window').width / 1 -6,     // Change number "2" - 6 to change element number for one row
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f1c40f'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  body: {
-    flex:1
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   }
 });
