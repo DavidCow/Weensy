32bf77310cf4ebd2159dd4005d4002b5def0c068
David Cao <davidcaone@gmail.com>
2019-01-23 20:17:44 +0100

    VideoContainerComponent VideoFeedComponent and Firebase npm install

--- src/scenes/CreateWeensyPage/index.js
+++ src/scenes/CreateWeensyPage/index.js
@@ -1,8 +1,6 @@
 /**
  * CREATE WEENSY PAGE
- * Shows Weensys and lets users watch the video
- * and tap a button inside the video to create
- * their own Weensy.
+ * 
  * @format
  * @flow
  */
@@ -12,40 +10,95 @@ import {
   StyleSheet, 
   Text, 
   View,
+  ScrollView,
+  Dimensions
 } from 'react-native';
-
+import Video from 'react-native-video';
+import { URL_PREFIX } from '../../../constants';
 
 
 export default class CreateWeensyPage extends Component {
+
+  componentWillMount() {
+    console.log("HALLO " + URL_PREFIX);
+    fetch(URL_PREFIX + "/videofeed.json")
+            .then(response => response.json() )
+            .then(data => {
+              console.log("Hallo du Namek"); 
+              console.log(data)
+            } )
+            .catch(error => console.log(error));
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Create a Weensy now!</Text>
-
-        <View style={styles.body}>
-        
-        </View>
-
-      </View>
+      <VideoContainerComponent url={{uri: "https://www.davidcaos.com/weensy/cap.mp4"}}></VideoContainerComponent>
     );
   }
 }
 
+/**
+ * VideoContainerComponent holds 10 videos 
+ * in a ScrollView.
+ */
+class VideoContainerComponent extends Component {
+  render() {
+    return (
+      <ScrollView style={styles.scrollContainer}>
+        <View style={styles.container}>
+                   <VideoSingleForFeed url={this.props.url} />
+                   <VideoSingleForFeed url={this.props.url} />
+                   <VideoSingleForFeed url={this.props.url} />
+        </View>
+      </ScrollView>
+    );
+  }
+}
+
+/**
+ * Display one single video
+ */
+class VideoSingleForFeed extends Component {
+  render() {
+    return (
+      <View style={styles.box}>
+              <Video source={this.props.url}  // Can be a URL or a local file.
+                    ref={(ref) => {
+                      this.player = ref
+                    }}                                      // Store reference
+                    onBuffer={this.onBuffer}                // Callback when remote video is buffering
+                    onError={this.videoError}               // Callback when video cannot be loaded
+                    style={styles.backgroundVideo} 
+                    repeat={false}/>
+       </View>
+    );
+  }
+}
+
+
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flex: 1,
+  },
   container: {
-    flex: 1
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    padding: 2,
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  box: {
+    margin: 2,
+    width: Dimensions.get('window').width / 1 -6,     // Change number "2" - 6 to change element number for one row
+    height: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f1c40f'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  body: {
-    flex:1
+  backgroundVideo: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
   }
 });
